76ff58218ffd7753c3230b5f5f9374247749e693
ZhihengLi0 <15101520162a@gmail.com>
2026-06-26 14:53:31 -0500

    Temp: increase batch size to 50000 for fast catch-up

--- sync_push.ps1
+++ sync_push.ps1
@@ -23,7 +23,7 @@ $RemoteDB   = "cs2"
 
 $StateFile = Join-Path $PSScriptRoot "win_sync_state.json"
 $LogFile   = Join-Path $PSScriptRoot "win_sync.log"
-$BatchSize = 5000
+$BatchSize = 50000
 
 $tables = @(
     "double_value_change_events",
